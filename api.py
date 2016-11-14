9eddc1371022d36861e7c6309fcf849231b3f16c
Stéphanie DESBY <stephanie.desby@vidal.fr>
2016-11-14 15:16:41 +0100

    Add API point /coordinates

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ from flask_restful import Api, Resource
 from webargs import fields
 from webargs.flaskparser import use_args
 import engine
+from geocode import Geocode
 
 app = Flask(__name__)
 api = Api(app)
@@ -23,7 +24,24 @@ class Distance(Resource):
         distance = engine.get_distance(args['x1'], args['y1'], args['x2'], args['y2'])
         return {"distance": distance}
 
+class Coordinates(Resource):
+
+    address = {
+    "number": fields.Str(missing=""),
+    "street": fields.Str(required=True),
+    "city": fields.Str(required=True)
+    }
+
+    @use_args(address)
+    def get(self, args):
+        geocode = Geocode()
+        address = args["number"] + " " + args["street"] + ", " + args["city"]
+        result = geocode.get_coordinates_from_address(address)
+
+        return result
+
 api.add_resource(Distance, "/distance")
+api.add_resource(Coordinates, "/coordinates")
 
 if __name__ == '__main__':
     app.run(debug=True)
