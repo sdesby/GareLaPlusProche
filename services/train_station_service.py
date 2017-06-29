05fa4ecb2aeb409cb4e981facc94cd9b8e683371
sdesby <desby.stephanie@gmail.com>
2017-06-29 12:53:46 +0200

    Add log on train station service

--- services/train_station_service.py
+++ services/train_station_service.py
@@ -1,7 +1,8 @@
 #coding: utf-8
-
+import garelaplusproche_logger as log
 from dao.database import Database
 
+LOGGER = log.get_logger("garelaplusproche.service")
 
 def get_all_train_stations():
     return Database().train_station_collection.find()
@@ -9,4 +10,5 @@ def get_all_train_stations():
 
 def get_nearest_train_station(user_latitude, user_longitude, maxdistance):
     max_distance = int(maxdistance) * 1000
+    LOGGER.info("Max Distance: " + str(max_distance))
     return Database().train_station_collection.find({"$and": [{"geometry": {"$near": {"$geometry": {"type": "Point", "coordinates":[user_longitude, user_latitude]},"$maxDistance": max_distance}}}, {"properties.voyageurs": "O"}]});
